bb3718326f8dcd0574bd45d09b76fb01660e8c2b
Harshkant87 <harshkant87@gmail.com>
2023-04-14 19:42:48 +0530

    First Commit

---
+++ myFirstDjangoProject/urls.py
@@ -0,0 +1,29 @@
+"""myFirstDjangoProject URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from myFirstDjangoProject import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.homePage), #homepage
+    path('about-us/', views.aboutUs), #connecting urls and views
+    path('courses/', views.Course),
+    path('contact/', views.contact),
+    path('courses/<int:courseid>', views.courseDetails), #we can use <str: var_name> or <slug: var_name> as well
+    # if you are not clear with the data type then simply pass the data e.g. courses/<courseid>
+
+]
---
+++ myFirstDjangoProject/views.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+from django.shortcuts import render #for rendering html pages 
+
+# def homePage(request):
+#     data = {
+#         'title': 'Home Page',
+#         'text_data': 'Hi My Name is Harsh Kant!',
+#         'skills': ['C', 'C++', 'Python'],
+#         'numbers': [10, 20, 30, 40, 50],
+#         'parents_details' : [
+#             {'name': 'Uday', 'Age': '45'},
+#             {'name': 'Niva', 'Age': '40'}
+#         ]
+#     }
+#     return render(request, "index.html" ,data) #rendering the homepage html page
+
+def homePage(request):
+    return render(request, "index.html") #rendering the homepage html page
+
+def aboutUs(request):
+    return render(request, "about.html") #defining views
+
+def Course(request):
+    return HttpResponse("<b> These are the courses we offer! </b>")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def courseDetails(request, courseid):
+    return HttpResponse(courseid)
